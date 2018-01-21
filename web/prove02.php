63a87d4cdad02fbf9392eeec53303feb346aecca
c8t12h19 <hol12062@byui.edu>
2018-01-20 20:22:18 -0700

    Edited style.css and prove02.php

--- web/prove02.php
+++ web/prove02.php
@@ -10,11 +10,14 @@
     <img src="profile-picture.jpg" alt="Engagement Photo">
     <br />
     <?php 
-      echo "<p id='intro'>Hi, I'm Cutler Hollist and I appreciate computer science
-        because of the incredible opportunities it provides to make this world a
-        better place. Some technologies I am interested in include the following:</p>";
+      echo "<p id='intro'>Hi, I'm Cutler Hollist, and the beautiful woman in the image
+        above is my wife! I wanted to share some of the things I appreciate most about
+        computer science because of the incredible opportunities it provides to make
+        this world a better place. <br /><br /> Some technologies I am interested in
+        include the following:</p>";
     ?>
-    <ul class="interests">
+    <ul class="interests" onmouseover="style.backgroundColor = '#4169e1'"
+        onmouseout="style.backgroundColor = 'white'">
       <li class="interests"><a href="http://sigmajs.org/" >Dataset Maps</a></li>
       <li class="interests"><a href="https://www.magicleap.com/" >Mixed Reality</a></li>
       <li class="interests"><a href="https://sovrin.org/" >Secure Systems</a></li>
